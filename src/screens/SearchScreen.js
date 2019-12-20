f6a70f71fe70a8f117c4263a4575de85031662c9
Shukti Shaikh <shukti.azad@gmail.com>
2019-12-20 12:04:37 -0500

    refactor hook logic

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,34 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
-import Yelp from "../api/Yelp";
+import useResults from "../hooks/useResults";
 
 const SearchScreen = () => {
 	const [term, setTerm] = useState("");
-	const [results, setResults] = useState([]);
-	const [errorMessage, setErrorMessage] = useState("");
-
-	const searchApi = async searchTerm => {
-		try {
-			const response = await Yelp.get("/search", {
-				params: {
-					limit: 50,
-					term: searchTerm,
-					location: "Boston"
-				}
-			});
-			setResults(response.data.businesses);
-		} catch (err) {
-			setErrorMessage("Something went wrong");
-		}
-	};
-
-	//Call searchApi when component is first rendered. BAD CODE!
-	//searchApi('pasta');
-
-	useEffect(() => {
-		searchApi("pasta");
-	}, []);
+	const [searchApi, results, errorMessage] = useResults();
 
 	return (
 		<View>
